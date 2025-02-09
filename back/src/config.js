7800051fc967f17de7516c48d4ff35d9620e827b
Alisson Ortega Martínez <alissonmartin2410@gmail.com>
2025-02-09 01:21:22 -0500

    Conection-Database

--- back/src/config.js
+++ back/src/config.js
@@ -1,17 +1,13 @@
-const { Sequelize } = require('sequelize'); // Se importa la clase Sequelize
-require('dotenv').config(); // Se importa la configuración de dotenv
+const { Sequelize } = require("sequelize");
 
-// Aquí se configura y establece la conexión 
-// a la base de datos utilizando Sequelize. 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,     // Nombre de la base de datos
-  process.env.DB_USER,     // Usuario
-  process.env.DB_PASSWORD, // Contraseña
-  {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'postgres',
-    logging: false, // Desactivar logs para mantener la consola limpia
-  }
-);
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
+});
 
 module.exports = sequelize;
