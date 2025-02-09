625bf3f868ad78a0d2861d9dbf430f527b706579
Alisson Ortega Martínez <alissonmartin2410@gmail.com>
2025-02-09 18:39:46 -0500

    dist

--- Front/src/pages/about/About.jsx
+++ Front/src/pages/about/About.jsx
@@ -7,7 +7,8 @@ const About = () => {
   return (
     <div className={styles.aboutContainer}>
       <div className={styles.imageContainer}>
-          <img
+{/*         Se usa la eqtiqueta img, para mostrar la imagen, usando los atributos src y alt.
+ */}          <img
             src={image}
             alt="Mensajería de EntregaYA"
             className={styles.serviceImage}
@@ -18,9 +19,6 @@ const About = () => {
         <p className={styles.description}>
           En EntregaYA, ofrecemos soluciones logísticas rápidas, seguras y confiables para el envío de paquetes físicos a nivel nacional e internacional. Nuestra misión es asegurar la entrega puntual y segura de cada paquete, garantizando una experiencia satisfactoria para todos nuestros clientes.
         </p>
-
-        
-
         <h3 className={styles.subtitle}>Misión</h3>
         <p className={styles.text}>
           Nuestra misión es brindar a nuestros clientes un servicio de mensajería confiable y puntual, utilizando tecnología avanzada y un equipo comprometido con la satisfacción del cliente.
@@ -30,9 +28,6 @@ const About = () => {
         <p className={styles.text}>
           Ser la empresa líder en el sector de mensajería física, proporcionando soluciones innovadoras y eficientes que mejoren la experiencia logística de nuestros clientes.
         </p>
-
-       
-
         <h3 className={styles.subtitle}>Por qué Elegir EntregaYA</h3>
         <p className={styles.text}>
           EntregaYA es la opción más confiable para tus envíos. Con un equipo altamente capacitado y un sistema de seguimiento en tiempo real, te garantizamos que tus paquetes llegarán a su destino de manera segura y puntual.
