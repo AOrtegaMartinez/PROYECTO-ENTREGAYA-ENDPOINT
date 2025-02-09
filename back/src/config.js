b28958af1743b95a38c914765bbd9f0a520d4484
Alisson Ortega Martínez <alissonmartin2410@gmail.com>
2025-02-09 02:11:29 -0500

    UpdateBD

--- back/src/config.js
+++ back/src/config.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
 const { Sequelize } = require("sequelize");
+
+console.log("DATABASE_URL:", process.env.DATABASE_URL); // Verifica que no sea undefined
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
