421742733faa5028e3276cfdbd2a90d3b174bd22
Alisson Ortega Martínez <alissonmartin2410@gmail.com>
2025-02-09 23:13:26 -0500

    fixviteConfig

--- Front/vite.config.js
+++ Front/vite.config.js
@@ -1,15 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { copyFileSync } from 'fs'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: "./", // Asegura rutas relativas en archivos generados
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-  },
-  // Hook para copiar el archivo _redirects a dist/ después del build
-  configResolved() {
-    copyFileSync('public/_redirects', 'dist/_redirects');
-  },
-})
+  }
+});
