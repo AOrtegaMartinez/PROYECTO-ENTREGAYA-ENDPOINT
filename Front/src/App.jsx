3993178ddbfafe09c642856390684030e8f1c3bb
Alisson Ortega Martínez <alissonmartin2410@gmail.com>
2025-02-09 23:20:58 -0500

    HashRouter

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 
 // Importamos los componentes y páginas necesarios
 import Navbar from './components/navbar/Navbar';
